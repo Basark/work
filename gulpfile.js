7fe1a9784de49df785da025f82fc4c342899afe0
Basark <mail@basark.de>
2019-02-17 02:03:55 +0100

    Added Concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 gulp.task ('log', function(done){
     gutil.log('Konsolenausgabe');
     done();
 });
 
-coffeeScripts = ['components/coffee/*.coffee'];
+var coffeeScripts = ['components/coffee/*.coffee'];
+var jsSources = ['components/scripts/*.js'];
 
 gulp.task('coffee', function(done){
     gulp.src(coffeeScripts)
     .pipe(coffee({bare: true})
     .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'));
+    done();
+});
+
+gulp.task('js', function(done){
+    gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('build/dev/js'));
     done();
 });
