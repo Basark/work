5d77ea5503ae440f93166d3f03e9b47a6a2e8dc7
Basark <mail@basark.de>
2019-02-17 02:16:31 +0100

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
 gulp.task ('log', function(done){
     gutil.log('Konsolenausgabe');
@@ -22,6 +23,7 @@ gulp.task('coffee', function(done){
 gulp.task('js', function(done){
     gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('build/dev/js'));
     done();
 });
